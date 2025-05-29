1b1219cd143a78c146e9805a9d8ad215f2976077
Alejandro Fernandez <alej.fernandez@duocuc.cl>
2025-05-29 17:51:59 -0400

    feat(router): lazy load AuthModule en app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // aquí añadimos “lazy load” para AuthModule:
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  // fallback
+  { path: '**', redirectTo: '' }
+];
