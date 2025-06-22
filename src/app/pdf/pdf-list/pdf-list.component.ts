5906a92a9018bf311c02bef8f62840743ddd6d7e
Alejandro Fernandez <alej.fernandez@duocuc.cl>
2025-06-22 13:35:18 -0400

    scaffold PdfModule, PdfListComponent y PdfViewerComponent

---
+++ src/app/pdf/pdf-list/pdf-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './pdf-list.component.html',
+  styleUrl: './pdf-list.component.css'
+})
+export class PdfListComponent {
+
+}
