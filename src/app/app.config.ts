19f6806280d60bf95abc9d2a725a0129c073e600
Alejandro Fernandez <alej.fernandez@duocuc.cl>
2025-06-25 19:37:58 -0400

    feat(router): switch to HTML5 routing, remove hashLocation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
@@ -7,7 +7,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()), 
+    provideRouter(routes), 
     provideAnimationsAsync(), 
     importProvidersFrom(HttpClientModule)
   ]
