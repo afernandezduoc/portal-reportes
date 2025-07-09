d1fe6de758e8f0ede830e1d3b63ac77e47623eee
Alejandro Fernandez <alej.fernandez@duocuc.cl>
2025-07-09 17:11:55 -0400

    Prueba de Proxy inverso desde Amplify + URL relativas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://3.92.188.187:8081'
+  apiUrl: ''
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://3.92.188.187:8081'    // pon aquí tu Public IPv4 o DNS
+  apiUrl: ''
 };
